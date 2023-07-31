555f612c8c75e4f7397bc90b9d44480c975b2792
Frank Kailey <114768013+ACIPASTA@users.noreply.github.com>
2023-07-31 20:58:41 +0300

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,3 @@ module.exports = async (client, user,message)  => {
   client.user.setActivity("ACIPASTA Reaksiyon bot !yardım")
   console.log("Bot Başarıyla Başlatıldı!")
 }
-
-// youtube.com/c/EmirhanSarac
